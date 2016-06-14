cd8c6b9cbb975855ecef130e4dcf72f77bee1660
Ruud Jansen <ruud.jansen@appsforagri.com>
2016-06-14 11:19:46 +0200

    Added another command

--- app.php
+++ app.php
@@ -3,8 +3,10 @@
 require __DIR__.'/vendor/autoload.php';
 
 use AppBundle\Console\Command\GreetCommand;
+use AppBundle\Console\Command\FooCommand;
 use Symfony\Component\Console\Application;
 
 $application = new Application();
 $application->add(new GreetCommand());
+$application->add(new FooCommand());
 $application->run();
