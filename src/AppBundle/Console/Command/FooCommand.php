e9d1ec8de48cb6aa1b21d8ee8b003e97a50e7cfe
Ruud Jansen <ruud.jansen@appsforagri.com>
2016-06-14 12:28:28 +0200

    Input options

--- src/AppBundle/Console/Command/FooCommand.php
+++ src/AppBundle/Console/Command/FooCommand.php
@@ -3,6 +3,7 @@
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputDefinition;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -13,31 +14,22 @@ class FooCommand extends Command
         $this
             ->setName('foo:bar')
             ->setDescription('Greet someone')
-            ->addArgument(
+            ->addOption(
                 'name',
+                'null',
                 InputArgument::OPTIONAL,
                 'Who do you want to greet?'
-            )
-            ->addOption(
-               'yell',
-               null,
-               InputOption::VALUE_NONE,
-               'If set, the task will yell in uppercase letters'
             )
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $name = $input->getArgument('name');
+        $name = $input->getOption('name');
         if ($name) {
             $text = 'Hello '.$name;
         } else {
             $text = 'Hello';
-        }
-
-        if ($input->getOption('yell')) {
-            $text = strtoupper($text);
         }
 
         $output->writeln($text);
